4d661d7bb70c6b16bb3e096a214585a359547958
Guuh017 <gs-nascimento@outlook.com>
2018-11-13 18:42:30 -0200

    v2.10

--- src/config/server.js
+++ src/config/server.js
@@ -1,15 +1,18 @@
-const port = 3003
+const port = 80
+
 const bodyParser = require('body-parser')
 const express = require('express')
+const server = express()
 const allowCors = require('./cors')
 const queryParser = require('express-query-int')
-const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
 server.use(queryParser())
 
-server.listen(port, () => console.log(`Climansys API rodando na porta ${port}.`))
+server.listen(process.env.PORT || port, () => {
+    console.log(`ClimansysAPI rodando na porta ${port}.`)
+})
 
 module.exports = server
